dd3d0a4757bb54659042550e2ea1b3c68eba130e
Wege0921 <wegemuluneh@gmail.com>
2023-07-13 14:32:23 -0700

    quote new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 (error)
  */
+
 int main(void)
-{	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(STDERR_FILENO, message, sizeof(message) - 1);
-
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
