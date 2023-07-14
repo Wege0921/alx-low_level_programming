580d1d6372ff63ba35a5799bc27b2805bd098548
Wege0921 <wegemuluneh@gmail.com>
2023-07-14 13:29:37 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = 97;
+
+	for (; num < 123; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
