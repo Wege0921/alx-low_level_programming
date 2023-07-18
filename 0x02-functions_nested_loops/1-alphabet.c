e54f2d6feb52af6e45cac2985cb90ba6bc13f7c4
Wege0921 <wegemuluneh@gmail.com>
2023-07-18 04:39:18 -0700

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 97; alpha < 123; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
