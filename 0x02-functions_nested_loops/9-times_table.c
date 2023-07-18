2bba9ed668c173ee0e34f19df6fb4f0de21389e1
Wege0921 <wegemuluneh@gmail.com>
2023-07-18 11:48:25 -0700

    9x table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int x, y, product;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			product = x * y;
+
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (product >= 10)
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
