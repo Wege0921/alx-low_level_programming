2fffb85117d8703f9f5f22f039f230a688257a56
Wege0921 <wegemuluneh@gmail.com>
2023-07-18 03:13:21 -0700

    putchar_

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main - check the code.
@@ -14,6 +15,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar("/n");
+	_putchar('\n');
 	return (0);
 }
