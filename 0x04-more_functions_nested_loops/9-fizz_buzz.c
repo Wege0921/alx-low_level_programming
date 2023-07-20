99e9528cb79f0d19a669b8b66801f874c6969293
Wege0921 <wegemuluneh@gmail.com>
2023-07-20 11:37:30 -0700

    fizzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
+		if (x % 15 == 0)
 			printf("FizzBuzz");
 		else if (x % 3 == 0)
 			printf("Fizz");
@@ -20,6 +20,8 @@ int main(void)
 			printf("Buzz");
 		else
 			printf("%d ", x);
+		if (i < 100)
+			printf(" ");
 	}
 
 	printf("\n");
