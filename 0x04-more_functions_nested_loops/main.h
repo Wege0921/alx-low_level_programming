e527a56c3374a6980cb75be41a9696ac3aed9d80
Wege0921 <wegemuluneh@gmail.com>
2023-07-20 09:16:46 -0700

    print nums

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* Add other function prototypes if needed */
+
+#endif /* MAIN_H */
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
