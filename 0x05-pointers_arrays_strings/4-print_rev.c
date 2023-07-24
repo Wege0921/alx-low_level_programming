ce7c58296155ad47c457bedbc71f437a9538af40
Wege0921 <wegemuluneh@gmail.com>
2023-07-24 11:55:49 -0700

    betty reverse str

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-
+/**
+ * print_rev - Prints a string in reverse followed by a new line.
+ * @s: The input string to be reversed and printed.
+ */
 void print_rev(char *s)
 {
-	int length = strlen(s)
+	int length = strlen(s);
 	int i;
 
 	for (i = length - 1; i >= 0; i--)
