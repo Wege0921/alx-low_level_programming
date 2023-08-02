170de01ee72784a266a5c4eb6943241ec3e28084
Wege0921 <wegemuluneh@gmail.com>
2023-08-02 14:35:11 -0700

    correction

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,45 +1,43 @@
 #include "main.h"
 
 /**
- * sqrt_recursive - Helper function to recursively calculate the square root.
- * @n: The number to calculate the square root of.
- * @i: The current guess for the square root.
+ * is_prime_recursive - Helper function to check if a number is prime.
+ * @n: The number to check.
+ * @i: The current divisor to check.
  *
- * Return: The square root of n, or -1 if n does not have a natural square root.
+ * Return: 1 if the number is prime, 0 otherwise.
  */
-int sqrt_recursive(int n, int i);
+int is_prime_recursive(int n, int i);
 
 /**
- * _sqrt_recursion - Calculate the square root of a number using recursion.
- * @n: The number to calculate the square root of.
+ * is_prime_number - Check if a number is prime.
+ * @n: The number to check.
  *
- * Return: The square root of n, or -1 if n does not have a natural square root.
+ * Return: 1 if the number is prime, 0 otherwise.
  */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
-    if (n < 0)
-        return -1;
+	if (n <= 1)
+		return (0);
 
-    if (n == 0 || n == 1)
-        return n;
-
-    return sqrt_recursive(n, 1);
+	return (is_prime_recursive(n, 2));
 }
 
 /**
- * sqrt_recursive - Helper function to recursively calculate the square root.
- * @n: The number to calculate the square root of.
- * @i: The current guess for the square root.
+ * is_prime_recursive - Helper function to check if a number is prime.
+ * @n: The number to check.
+ * @i: The current divisor to check.
  *
- * Return: The square root of n, or -1 if n does not have a natural square root.
+ * Return: 1 if the number is prime, 0 otherwise.
  */
-int sqrt_recursive(int n, int i)
+int is_prime_recursive(int n, int i)
 {
-    if (i * i == n)
-        return i;
+	if (i <= n / 2)
+	{
+		if (n % i == 0)
+			return (0);
+		return (is_prime_recursive(n, i + 1));
+	}
 
-    if (i * i > n)
-        return -1;
-
-    return sqrt_recursive(n, i + 1);
+	return (1);
 }
