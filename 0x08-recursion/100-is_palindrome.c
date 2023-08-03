c16291d79a0dff028c1ebbd8d11ab2f62ff42208
Wege0921 <wegemuluneh@gmail.com>
2023-08-03 13:25:12 -0700

    palindrom

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,21 @@
 #include "main.h"
 #include <stddef.h>
+
+int is_palindrome_recursive(char *s, int start, int end);
+
+/**
+ * _strlen - Calculate the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+
 /**
  * is_palindrome - Check if a string is a palindrome.
  * @s: The input string to check.
@@ -8,20 +24,24 @@
  */
 int is_palindrome(char *s)
 {
-	int len = 0;
-	int i, j;
+	int len = _strlen(s);
+	return (is_palindrome_recursive(s, 0, len - 1));
+}
 
-	if (s == NULL)
+/**
+ * is_palindrome_recursive - Helper function to check
+ *	if a string is a palindrome.
+ * @s: The input string.
+ * @start: The starting index.
+ * @end: The ending index.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int is_palindrome_recursive(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
 		return (0);
-
-	while (s[len] != '\0')
-		len++;
-
-	for (i = 0, j = len - 1; i < len / 2; i++, j--)
-	{
-		if (s[i] != s[j])
-			return (0);
-	}
-
-	return (1);
+	return (is_palindrome_recursive(s, start + 1, end - 1));
 }
