4f4e2200d557b3de331f90c68d773ce226c8b31d
Wege0921 <wegemuluneh@gmail.com>
2023-08-04 17:03:16 -0700

    mul2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,37 +1,72 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * _mul - Converts a string to an integer
- * @s: The string to convert
+ * string_to_int - converts a string to an integer
+ * @str: string to be converted
  *
- * Return: The converted integer, or 0
- *	if no valid conversion could be performed
+ * Return: the integer converted from the string
  */
-int _mul(char *s)
+int string_to_int(char *str)
 {
-	int i = 0, sign = 1, result = 0;
+	int i, sign, num, len, found_digit;
 
-	/* Skip leading whitespace */
-	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-		i++;
+	i = 0;
+	sign = 0;
+	num = 0;
+	len = 0;
+	found_digit = 0;
 
-	/* Handle sign */
-	if (s[i] == '-')
+	while (str[len] != '\0')
+		len++;
+
+	while (i < len && !found_digit)
 	{
-		sign = -1;
+		if (str[i] == '-')
+			sign++;
+
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			int digit = str[i] - '0';
+			if (sign % 2)
+				digit = -digit;
+			num = num * 10 + digit;
+			found_digit = 1;
+			if (str[i + 1] < '0' || str[i + 1] > '9')
+				break;
+			found_digit = 0;
+		}
 		i++;
 	}
-	else if (s[i] == '+')
-		i++;
 
-	/* Convert digits */
-	while (s[i] >= '0' && s[i] <= '9')
+	if (!found_digit)
+		return (0);
+
+	return (num);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, operand1, operand2;
+
+	if (argc != 3)
 	{
-		result = result * 10 + (s[i] - '0');
-		i++;
+		printf("Error\n");
+		return (1);
 	}
 
-	return (result * sign);
+	operand1 = string_to_int(argv[1]);
+	operand2 = string_to_int(argv[2]);
+	result = operand1 * operand2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-int _mul(char *s);
+int string_to_int(char *str);
 int is_positive_number(char *s);
 #endif /* MAIN_H */
