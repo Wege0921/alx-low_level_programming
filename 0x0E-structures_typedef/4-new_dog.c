2978411572e39d436833c1a3e88243ded0209b21
Wege0921 <wegemuluneh@gmail.com>
2023-08-14 05:22:48 -0700

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,10 +12,12 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
