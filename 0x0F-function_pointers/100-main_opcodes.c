512f4ed5e9e7bc043c66001f1d264a8b9f57fc29
Wege0921 <wegemuluneh@gmail.com>
2023-08-16 11:49:05 -0700

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the opcodes of its own main function.
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	unsigned char *ptr = (unsigned char *)main;
+	int i;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", ptr[i]);
+		if (i < bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
