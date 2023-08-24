cc8197035d50f004de25d705989461c90ec06f60
Wege0921 <wegemuluneh@gmail.com>
2023-08-24 12:49:20 -0700

    100-first betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * pre_main - Function executed before the main function.
+ *
+ * Description: This function is executed automatically before the main
+ * function is called. It prints a predefined message.
+ */
 
 void __attribute__ ((constructor)) pre_main() {
 	printf("You're beat! and yet, you must allow,\n"
