ebf31e18b0bab296a340267a6ac03e3b711da69f
Wege0921 <wegemuluneh@gmail.com>
2023-08-29 10:43:14 -0700

    free list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,56 +1,41 @@
-#include <stddef.h>
-#include <stdlib.h>
 #include "lists.h"
+
 /**
- * free_listint_safe - Frees a listint_t linked list.
+ * free_listint_safe - Safely frees a listint_t linked list.
  * @h: Double pointer to the head of the linked list.
  *
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *slow, *fast, *temp;
-	size_t count = 0;
+	size_t list_size = 0; /* To store the size of the list */
+	int diff; /* To calculate the difference between pointers */
+	listint_t *temp; /* Temporary pointer to hold the next node */
 
-	if (h == NULL || *h == NULL)
+	if (!h || !*h) /* Check if the list or head is NULL */
 		return (0);
 
-	slow = *h;
-	fast = *h;
-
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	while (*h)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
+		diff = *h - (*h)->next; /* Calculate the difference between pointers */
+		if (diff > 0) /* If the difference is positive, no loop detected */
 		{
-			while (*h != slow)
-			{
-				temp = *h;
-				*h = (*h)->next;
-				free(temp);
-				count++;
-			}
-			while (slow != NULL)
-			{
-				temp = slow;
-				slow = slow->next;
-				free(temp);
-				count++;
-			}
-			*h = NULL;
-			return (count);
+			temp = (*h)->next; /* Store the next node in temp */
+			free(*h); /* Free the current node */
+			*h = temp; /* Move head to the next node */
+			list_size++; /* Increment the list size */
+		}
+		else /* Loop detected or single node remaining */
+		{
+			free(*h); /* Free the current node */
+			*h = NULL; /* Set head to NULL */
+			list_size++; /* Increment the list size */
+			break; /* Exit the loop */
 		}
 	}
 
-	while (*h != NULL)
-	{
-		temp = *h;
-		*h = (*h)->next;
-		free(temp);
-		count++;
-	}
+	*h = NULL; /* Set head to NULL after freeing all nodes */
 
-	return (count);
+	return (list_size); /* Return the size of the freed list */
 }
+
